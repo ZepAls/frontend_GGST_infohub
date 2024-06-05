0f6f97d8764b51a7f53c1178eba272c5b501fcc4
ZepAlsters <94059018+ZepAlsters@users.noreply.github.com>
2024-06-05 16:04:39 +0200

    Add project files.

---
+++ frontend_GGST_infohub/DTO/ProposalDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace frontend_GGST_infohub.DTO
+{
+    public class ProposalDTO
+    {
+        public int id { get; set; }
+        public string character { get; set; }
+        public bool accepted { get; set; }
+        public string paragraphName { get; set; }
+        public string text { get; set; }
+    }
+}
